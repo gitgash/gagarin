f1f788417f536aa0a46235c4a1de212a5abff16c
Oleg Gashnikov <oleg.gashnikov@progresspoint.ru>
2012-09-05 03:11:28 +0400

    snippet

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -49,7 +49,7 @@ class ResultController < ApplicationController
       
       @results = Array.new
       sec.xpath("result").each do |r|
-        @results << {:url => r.xpath('url'), :title => r.xpath('title'), :url_show => r.xpath('url_show'), :title_show => r.xpath('title_show')}
+        @results << {:url => r.xpath('url'), :title => r.xpath('title'), :url_show => r.xpath('url_show'), :title_show => r.xpath('title_show'), :snippet => r.xpath('snippet')}
       end
     end
     @section_code=params[:id]
