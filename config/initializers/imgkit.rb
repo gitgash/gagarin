ad1290168f4437b9df5f49bd9d2ed86bcd0c4878
Aleksandr Salkov <asalkov@at-consulting.ru>
2012-09-05 03:22:03 +0400

    Вынес код страниц в отдельные паршилы

--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,7 +1,8 @@
 IMGKit.configure do |config|
   #config.wkhtmltoimage = '/usr/local/bin'
   config.default_options = {
-    :quality => 60
+    :quality => 60,
+    :width => 630,
   }
   config.default_format = :jpg
 end
