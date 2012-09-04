81fe40123edfa7d44c9673dac09e6e8567da3025
Oleg Gashnikov <oleg.gashnikov@progresspoint.ru>
2012-09-04 23:58:58 +0400

    добавил section_code

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -18,9 +18,10 @@ class ResultController < ApplicationController
       doc  = Nokogiri::XML(response.body.to_s)
       
       #collect sections
+      uri = URI(request.url)
       doc.xpath('//query').each do |q|
         ret << "#{result_section_path(:id => q['collection'])}.jpg?search=#{URI.encode params[:search]}"
-        ret << "#{result_section_path(:id => q['collection'])}.html?search=#{URI.encode params[:search]}"
+        ret << "#{uri.scheme}://#{uri.host}#{result_section_path(:id => q['collection'])}.html?search=#{URI.encode params[:search]}"
       end
 
     end
@@ -47,6 +48,7 @@ class ResultController < ApplicationController
         @results << {:url => r.xpath('url'), :title => r.xpath('title'), :url_show => r.xpath('url_show'), :title_show => r.xpath('title_show')}
       end
     end
+    @section_code=params[:id]
     
     respond_to do |format|
       format.html
@@ -54,17 +56,12 @@ class ResultController < ApplicationController
         #let imgkit load html from current host and convert
         #write jpg out
         
-        p "DEBUGG>>> request.url="+request.url
         uri = URI(request.url)
         s = uri.path
-        p "DEBUGG>>> s="+s
         s['.jpg'] = '.html'
         uri.path = s
         uri.port = 3003
-        p "DEBUGG>>> uri="+uri.to_s
         contents = IMGKit.new(uri.to_s)
-        #contents = IMGKit.new("http://www.google.com")
-        #File.open("img.jpg", 'w') {|f| f.write(contents.to_img(:jpg)) }
         send_data contents.to_img(:jpg), :filename => "#{params[:id]}.jpg", 
                                         :disposition => 'inline', 
                                         :type => "image/jpg"
