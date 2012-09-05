5e3bc6f75fcb679fa270198335ba5a97d7963e69
Никита Северинов <nikita.severinov@gmail.com>
2012-09-05 20:44:43 +0400

    Добавление <div>-ов

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -51,7 +51,7 @@ function search(){
 		      carusel.innerHTML+="<a target='_self' href='http://"+data[i+1]+"'><img class = 'cloudcarousel' src='"+(data[i])+"' /></a>";		
 	  	  }
 		  init_carusel();
-		  // carusel.style.display = "block";
+		  carusel.style.display = "block";
 	   }
 	  });
   }
