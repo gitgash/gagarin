e3669bbad4f42ce90513055d56b6ccc79851a9b4
Никита Северинов <nikita.severinov@gmail.com>
2012-09-05 21:28:50 +0400

    Добавление iframe>-ов

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -53,7 +53,7 @@ function search(){
           id_page = Math.random()*5;
 		      //carusel.innerHTML+="<a target='_self' href='http://"+data[i+1]+"'><img class = 'cloudcarousel' src='"+(data[i])+"' /></a>";		
 	  	    carusel.innerHTML+="<img class = 'cloudcarousel' src='"+(data[i])+"' onClick='unvisible(\""+(data[i+1])+"\","+id_page+")'/>";
-          result.innerHTML+="<iframe id='"+id_page+"' src=\"http://"+(data[i+1])+"\" width=\"630px\" height='500px' class='l-projects_shadow' style=\"visibility:hidden;\">Ваш браузер не поддерживает iframe</iframe>";
+          result.innerHTML+="<iframe id='"+id_page+"' src=\"http://"+(data[i+1])+"\" width=\"630px\" height='500px' class='l-projects_shadow' style=\"visibility:hidden;z-index:10000;\">Ваш браузер не поддерживает iframe</iframe>";
         }
 		  init_carusel();
 	   }
