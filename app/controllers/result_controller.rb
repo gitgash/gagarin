ecee4542078042c972d11d6486fc636bde043c29
Oleg Gashnikov <oleg.gashnikov@progresspoint.ru>
2012-09-07 00:01:12 +0400

    Ускрение первого чтения

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -44,7 +44,11 @@ class ResultController < ApplicationController
           "/result/section/31.jpg?search=xuyandzhopenotfound", "localhost:3000/result/section/31.html?search=xuyandzhopenotfound"
         ]
       end
-
+      
+      #save this object for later use
+      keys = URI::split(request.url)
+      key="#{keys[5]}/#{keys[7]}"
+      Rails.cache.write(key, response.body.to_s)
     end
     
     respond_to do |format|
@@ -53,47 +57,47 @@ class ResultController < ApplicationController
   end
   
   def section
-    http_session = Net::HTTP.new('3d.sputnik.ru', 80)
-    http_session.start do |http| 
-      req = Net::HTTP::Get.new("/search?q=#{URI.encode params[:search]}&xmlout=1")
-      # req.basic_auth 'admin', 'SearchRF'
-      req.basic_auth 'html5', 'KvorumSLA'
-      response = http.request(req)
-      
-      doc  = Nokogiri::XML(response.body.to_s)
-      
-      #get section
-      sec = doc.xpath("//kmsearch[query[@collection=#{params[:id]}]]")
-      
-      @results = Array.new
-      sec.xpath("result").each do |r|
-        @results << {
-          :url => r.xpath('url'), 
-          :title => r.xpath('title'), 
-          :url_show => r.xpath('url_show'), 
-          :title_show => r.xpath('title_show'), 
-          :snippet => r.xpath('snippet'),
-          :datetime => r.xpath('datetime'),
-          :image_url =>
-            # обязательно переделать эту хрень!!!
-            if r.xpath('user_data').empty?
-              nil
-            elsif r.xpath('user_data').xpath('image_data').empty?
-              if r.xpath('user_data').xpath('video_data').empty?
-                nil
-              else
-                r.xpath('user_data').xpath('video_data').attribute('thumbnail')
-              end
-            else           
-              r.xpath('user_data').xpath('image_data').attribute('url')
-            end,
-        }
-      end
-    end
-    @section_code=params[:id]
-    
+  
     respond_to do |format|
-      format.html
+      format.html {
+        #read saved sputnik responce
+        keys = URI::split(request.url)
+        key="#{keys[5]}/#{keys[7]}"
+        xml = Rails.cache.read(key)
+        
+        doc  = Nokogiri::XML(xml)
+
+        #get section
+        sec = doc.xpath("//kmsearch[query[@collection=#{params[:id]}]]")
+
+        @results = Array.new
+        sec.xpath("result").each do |r|
+          @results << {
+            :url => r.xpath('url'), 
+            :title => r.xpath('title'), 
+            :url_show => r.xpath('url_show'), 
+            :title_show => r.xpath('title_show'), 
+            :snippet => r.xpath('snippet'),
+            :datetime => r.xpath('datetime'),
+            :image_url =>
+              # обязательно переделать эту хрень!!!
+              if r.xpath('user_data').empty?
+                nil
+              elsif r.xpath('user_data').xpath('image_data').empty?
+                if r.xpath('user_data').xpath('video_data').empty?
+                  nil
+                else
+                  r.xpath('user_data').xpath('video_data').attribute('thumbnail')
+                end
+              else           
+                r.xpath('user_data').xpath('image_data').attribute('url')
+              end,
+          }
+        end
+        @section_code=params[:id]
+        
+        render
+        }
       format.jpg  {
         #let imgkit load html from current host and convert
         #write jpg out
